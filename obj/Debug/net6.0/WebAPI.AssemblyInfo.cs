47a4ca6be7fdee5ddc2085b0c251eaf6410752d8
Andrew Ribeiro Silva <1246455@ifmt.local>
2023-03-22 14:50:25 -0400

    insert docker file

--- obj/Debug/net6.0/WebAPI.AssemblyInfo.cs
+++ obj/Debug/net6.0/WebAPI.AssemblyInfo.cs
@@ -11,6 +11,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("92434fd5-59fe-47a9-b5da-cacb4a47b595")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
